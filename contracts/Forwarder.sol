3d73c342788b51bb70256a607161a0550c094a20
Sanbao Zheng <baobaobao369@163.com>
2023-11-02 12:05:08 +0800

    init with mud

--- contracts/Forwarder.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.11;
-
-import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
-
-/**
- * Local minimal trusted forwarder for metatransactions
- */
-contract Forwarder is MinimalForwarder {}
